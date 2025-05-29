f18d7d3b0ca0257b34c167d5fcd17ca28527568f
AlejandroBR10 <alejabaldera@gmail.com>
2025-05-28 21:12:06 -0600

    Agregando endpoints al proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MovieModule } from './movie/movie.module';
+import { ShowtimeModule } from './showtime/showtime.module';
+import { SeatModule } from './seat/seat.module';
+import { AuthModule } from './auth/auth.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, MovieModule, ShowtimeModule, SeatModule, TicketModule],
   controllers: [AppController],
   providers: [AppService],
 })
